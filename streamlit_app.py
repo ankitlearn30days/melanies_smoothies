2dcca4732ef3985ecec32fd6996d0d9e427fc99b
ankitlearn30days <ankit41298@gmail.com>
2025-11-15 23:48:01 +0530

    Update streamlit_app.py
    Changes added to remove dataset

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,8 +37,8 @@ if ingredients_list:
 
     #st.write(my_insert_stmt)
     #st.stop()
-    smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-    sf_df = st.dataframe(data = smoothiefroot_response.json(), use_container_width = True)
+    #smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+    #sf_df = st.dataframe(data = smoothiefroot_response.json(), use_container_width = True)
     
     time_to_insert = st.button('Submit Order')
     if time_to_insert:
